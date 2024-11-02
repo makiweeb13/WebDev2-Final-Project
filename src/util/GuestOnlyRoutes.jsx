06f8b61901cf624f4f7b2274ea821160d7236b56
makiweeb13 <mariamaekiskis13@gmail.com>
2024-11-02 21:01:58 +0800

    guest only routes

--- src/util/GuestOnlyRoutes.jsx
+++ src/util/GuestOnlyRoutes.jsx
@@ -1,14 +1,11 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from './useAuth';
 
 const GuestOnlyRoutes = () => {
   const { isAuthenticated, loading } = useAuth();
-  const location = useLocation();
-
-  const restricted = ['/login', '/signup'];
 
   if (!loading) {
-    return isAuthenticated && !restricted.includes(location.pathname) ? <Outlet /> : <Navigate to="/" />;
+    return !isAuthenticated ? <Outlet /> : <Navigate to="/" />;
   }
 };
 
